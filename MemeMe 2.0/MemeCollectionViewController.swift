bc1a8064c8dc0cc9f1f563116ea4220d5b52d83e
[01;31m[K"name"[m[K: "Phillip Crawford <[01;31m[K"email"[m[K: "phillipcrawford3@gmail.com>
2016-07-20 06:55:00 -0700

    flesh out MemeCollectionViewController. rough functionality of project done

--- MemeMe 2.0/MemeCollectionViewController.swift
+++ MemeMe 2.0/MemeCollectionViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MemeCollectionViewController: UICollectionViewController {
     
+
+    @IBOutlet var imageCollectionView: UICollectionView!
     var memes: [Meme] {
         return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
     }
@@ -17,5 +19,28 @@ class MemeCollectionViewController: UICollectionViewController {
     override func viewDidLoad(){
         super.viewDidLoad()
     }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
     
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MemeCollectionViewCell", forIndexPath: indexPath) as! MemeCollectionViewCell
+        let meme = memes[indexPath.item]
+        let imageView = UIImageView(image: meme.memedImage)
+        cell.backgroundView = imageView
+        return cell
+    }
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let object: AnyObject = self.storyboard!.instantiateViewControllerWithIdentifier("MemeDetailViewController")
+        let detailVC = object as! MemeDetailViewController
+        detailVC.meme = self.memes[indexPath.item]
+        navigationController!.pushViewController(detailVC, animated: true)
+    }
 }
+
